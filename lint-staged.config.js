971057ef1260bdf71b3ba6d690c584891f185840
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2024-11-13 14:29:00 +0100

    fix: config update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  '**/*.{ts,tsx}?(x)': () => 'npm run check-types',
-  '*.json': ['prettier --write']
+  '**/*.{ts,tsx}?(x)': () => 'npm run check-types'
 }
