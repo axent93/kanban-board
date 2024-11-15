2e6ef1065e4aaa4970a9c55d27314cac18e0e4b1
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2024-11-15 20:02:13 +0100

    feat: running unit tests in pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import './App.scss'
 
-import logo from './logo.svg'
+import Board from './components/Board/Board'
+import Search from './components/Search/Search'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+    <main className='app'>
+      <header className='app__header'>
+        <Search />
       </header>
-    </div>
+      <Board />
+    </main>
   )
 }
 
