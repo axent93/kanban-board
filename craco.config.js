751acf5e19489e669db7cc481486f063211fe1c5
Milos Aksentijevic <milos.aksentijevic@quantox.com>
2024-11-19 20:48:56 +0100

    fix: updated alias paths configuration

--- craco.config.js
+++ craco.config.js
@@ -5,7 +5,11 @@ const path = require(`path`)
 module.exports = {
   webpack: {
     alias: {
-      '@/': path.resolve(__dirname, 'src/')
+      '@/': path.resolve(__dirname, 'src/'),
+      '@/store': path.resolve(__dirname, 'src/store/'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@/data': path.resolve(__dirname, 'src/data/'),
+      '@/styles': path.resolve(__dirname, 'src/styles/')
     }
   }
 }
--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,11 @@ module.exports = {
   moduleNameMapper: {
     // Mock SCSS and CSS imports
     '\\.(css|scss)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/store/(.*)$': '<rootDir>/src/store/$1',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@/data/(.*)$': '<rootDir>/src/data/$1',
+    '^@/styles/(.*)$': '<rootDir>/src/styles/$1'
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
